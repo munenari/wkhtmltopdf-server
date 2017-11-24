f8a41bb121c5ba89d5bb511d98ee72e99a22aaec
munenari <munenari@gmail.com>
2017-11-24 15:35:05 +0900

    Delete darwin and windows binary

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 GOOS=linux GOARCH=amd64 go build -o bin/wkhtmltopdf-server-linux-amd64
 
-GOOS=darwin GOARCH=amd64 go build -o bin/wkhtmltopdf-server-darwin-amd64
+#GOOS=darwin GOARCH=amd64 go build -o bin/wkhtmltopdf-server-darwin-amd64
 
-GOOS=windows GOARCH=amd64 go build -o bin/wkhtmltopdf-server-windows-amd64.exe
+#GOOS=windows GOARCH=amd64 go build -o bin/wkhtmltopdf-server-windows-amd64.exe
